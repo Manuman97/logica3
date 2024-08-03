175d0d3b91c3555ddb55492253951c76eb86599c
MANUEL GOMEZ LOBO ROMERO <josema1976@protonmail.comgit config --global user.email>
2024-08-03 22:11:10 +0200

    phyton adivina el numero

--- pruebaphytongit/prueba.py
+++ pruebaphytongit/prueba.py
@@ -1,1 +1,31 @@
-print("hola mundo")
+# Este es el juego de adivinar el numero
+numero = 38
+intentos = 1
+
+print("Ingrese un numero del 1 al 50")
+num = int(input()) # pedimos un numero
+if(num==numero):
+    print("El numero a sido adivinado")
+    print("utilizaste {intentos}")
+else:
+    if(num < numero):
+            print("Te has quedado corto")
+    else:
+         print("Te has pasado")
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
